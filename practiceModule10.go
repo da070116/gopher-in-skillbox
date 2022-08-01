7470b06cbefe3e343c352d338cfe4deb55e08fdf
Alexander Dubrovin <da070116@gmail.com>
2022-08-01 23:25:22 +0300

    Add task2

--- practiceModule10.go
+++ practiceModule10.go
@@ -5,7 +5,7 @@ import (
 	"math"
 )
 
-func main() {
+func taylorSeriesForExponent() {
 	fmt.Println("Задание 1. Разложение ex в ряд Тейлора")
 	/*
 		E^x = Sum(x^n/n!)
@@ -18,7 +18,7 @@ func main() {
 	fmt.Print("Введите значение переменной х: ")
 	_, _ = fmt.Scan(&x)
 
-	epsilon := 1 / math.Pow(10, x)
+	epsilon := 1 / math.Pow(10, float64(myPrecious))
 
 	funcResult := float64(0)
 	funcPrevResult := float64(0)
@@ -35,5 +35,45 @@ func main() {
 		}
 		n++
 		funcPrevResult = funcResult
+
 	}
+
+	fmt.Printf("Чтобы добиться требуемой точности %v понадобилось %v итераций\n", epsilon, n+1)
+	fmt.Println("=========================================")
+}
+
+func percentRoundingTroubles() {
+	var clientBaseSum float64
+	var yearsToKeep uint8
+	var monthInterestRate uint8
+
+	fmt.Print("Сумма вклада: ")
+	fmt.Scan(&clientBaseSum)
+	fmt.Print("Сколько (целых) лет хранить вклад: ")
+	fmt.Scan(&yearsToKeep)
+	fmt.Print("Ежемесячная процентная ставка: ")
+	fmt.Scan(&monthInterestRate)
+
+	resultSum := clientBaseSum
+	auxiliarySum := float64(0)
+
+	bankInterest := float64(0)
+
+	for i := 0; i < int(yearsToKeep)*12; i++ {
+		capitalization := resultSum * (float64(monthInterestRate) / 100)
+		resultSum += capitalization
+
+		fmt.Println(capitalization * 100)
+		auxiliarySum = math.Trunc(auxiliarySum)
+		fmt.Println(auxiliarySum)
+		auxiliarySum = capitalization / 100
+		fmt.Println("aux", auxiliarySum)
+		bankInterest += capitalization - auxiliarySum
+	}
+	fmt.Println(resultSum, bankInterest)
+}
+
+func main() {
+	//taylorSeriesForExponent()
+	percentRoundingTroubles()
 }
