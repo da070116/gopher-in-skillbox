ed1655fdff2ef0ede98b4569970e63e8beeb03bb
alexander.dubrovin <alexander.dubrovin@osinit.com>
2022-08-22 18:17:35 +0300

    Module 27 in process

--- main.go
+++ main.go
@@ -1,93 +1,36 @@
 package main
 
 import (
-	"flag"
+	"bufio"
 	"fmt"
-	"io"
 	"os"
+	"os/signal"
 	"strings"
 )
 
-func manage(attrs []string) (result string) {
-	// manage -- возвращает результат в зависимости от числа аргументов на входе
-	switch {
-	case len(attrs) == 1 || len(attrs) == 2:
-		result = readFileData(attrs...)
-	case len(attrs) == 3:
-		data := readFileData(attrs[:2]...)
-		writeToFile(attrs[2], data)
+type Student struct {
+	name  string
+	age   int
+	grade int
+}
 
-	default:
-		panic("Error: use 1, 2 or 3 args")
-	}
+// newStudent - создание новой сущности типа Student из необработанной строки
+func newStudent(rawData string) (st Student) {
 	return
 }
 
-func writeToFile(fileName string, data string) {
-	file, err := os.Create(fileName)
-	if err != nil {
-		panic(err)
-	}
-	defer func(file *os.File) {
-		err := file.Close()
-		if err != nil {
-
-		}
-	}(file)
-	fmt.Printf("write %+v into %v\n", data, fileName)
-	_, err = file.WriteString(data)
-	if err != nil {
-		panic(err)
-	}
-
-}
-
-func readFromFile(fileName string) []byte {
-	// проверить наличие:
-	f, err := os.Open(fileName)
-	if err != nil {
-		fmt.Printf("Файл %v не найден\n", fileName)
-		return nil
-	}
-	defer func(f *os.File) {
-		err := f.Close()
-		if err != nil {
-			panic(err)
-		}
-	}(f)
-
-	// проверить размер:
-	fileInfo, err := f.Stat()
-	if err != nil || fileInfo.Size() == 0 {
-		fmt.Printf("Файл %v пуст\n", fileInfo.Name())
-		return nil
-	}
-	// считать в буфер по размеру файла:
-	buf := make([]byte, fileInfo.Size())
-	if _, err := io.ReadFull(f, buf); err != nil {
-		panic(err)
-	}
-	return buf
-}
-
-func readFileData(fileNames ...string) (result string) {
-	//readFileData -- прочитать данные из одного или нескольких файлов, результат объединить в строку
-	data := make([]string, len(fileNames))
-	for _, name := range fileNames {
-		fileContent := readFromFile(name)
-		if fileContent != nil {
-			data = append(data, string(fileContent))
-		}
-	}
-	result = strings.Join(data, "\n")
-	result = strings.Trim(result, "\n")
-	return
-}
-
+// main - выполнение основной логики программы
 func main() {
-	// получить параметры без имени из аргументов запуска
-	flag.Parse()
-	var rawParameters = flag.Args()
+	for {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt)
 
-	fmt.Printf("%v\n", manage(rawParameters))
+		inputSource := bufio.NewReader(os.Stdin)
+		rawInput, _ := inputSource.ReadString('\n')
+		rawInput = strings.Trim(rawInput, "\n")
+		fmt.Printf("->%v<-\n", rawInput)
+
+		fmt.Println("Go down")
+		break
+	}
 }
