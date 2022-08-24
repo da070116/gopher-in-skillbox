eb7490942409d8055ba854b6d746948023d76942
Alexander Dubrovin <da070116@gmail.com>
2022-08-24 23:08:07 +0300

    Task 27 ready

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io"
 	"os"
 	"strconv"
 	"strings"
@@ -15,30 +14,31 @@ type Student struct {
 	grade int
 }
 
-// strToInt - преобраозовать строку в число
+// strToInt - преобразовать строку в число
 func strToInt(value string) (result int) {
+	value = strings.Trim(value, " ")
+	result, err := strconv.Atoi(value)
+	if err != nil {
+		fmt.Println("Ошибка:", err)
+		result = -1
+	}
 	return
 }
 
 // newStudent - создание новой сущности типа Student из необработанной строки
-func newStudent(rawData string) (st Student) {
+func newStudent(rawData string) (err any, st Student) {
 	items := strings.Split(rawData, " ")
+
 	st.name = items[0]
-	_age, err := strconv.Atoi(items[1])
-	if err != nil {
-		fmt.Println("Ошибка:", err)
-		_age = 0
+	st.age = strToInt(items[1])
+	st.grade = strToInt(items[2])
+	if st.age == -1 || st.grade == -1 {
+		err = -1
 	}
-	st.age = _age
-	_grade, err := strconv.Atoi(items[2])
-	if err != nil {
-		fmt.Println("Ошибка:", err)
-		_grade = 0
-	}
-	st.age = _grade
 	return
 }
 
+// studentInfo - вывод данных о структуре Student
 func (s Student) studentInfo() (info string) {
 	info = fmt.Sprintf("Student %v (age: %d) of grade %d\n", s.name, s.age, s.grade)
 	return
@@ -46,14 +46,32 @@ func (s Student) studentInfo() (info string) {
 
 // main - выполнение основной логики программы
 func main() {
-	for {
+	const EOT = 4
+	counter := 0
+	studentsStorage := make(map[int]Student)
 
+	for {
 		inputSource := bufio.NewReader(os.Stdin)
-		rawInput, err := inputSource.ReadString('\n')
-		if err == io.EOF {
+		fmt.Print("Введите данные о студенте в формате `имя` `возраст` `курс`: ")
+		rawInput, _ := inputSource.ReadString('\n')
+		rawInput = strings.Trim(rawInput, "\r\n")
+		if rune(rawInput[0]) == EOT {
+			fmt.Println("Пользователь запросил выход")
 			break
 		}
-		rawInput = strings.Trim(rawInput, "\n")
-		fmt.Println("Go down")
+
+		err, student := newStudent(rawInput)
+		if err != nil {
+			fmt.Println("Ошибка при вводе данных")
+		} else {
+			studentsStorage[counter] = student
+			fmt.Println("Запись внесена")
+		}
+		counter++
 	}
+	fmt.Println("Хранилище студентов содержит записи:")
+	for i := range studentsStorage {
+		fmt.Print(studentsStorage[i].studentInfo())
+	}
+
 }
