9583f333a7a2a7c8cf79f6cb0dabcc975fc82446
alexander.dubrovin <alexander.dubrovin@osinit.com>
2022-08-24 18:52:20 +0300

    Module 27 in process

--- main.go
+++ main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
-	"os/signal"
+	"strconv"
 	"strings"
 )
 
@@ -14,23 +15,45 @@ type Student struct {
 	grade int
 }
 
+// strToInt - преобраозовать строку в число
+func strToInt(value string) (result int) {
+	return
+}
+
 // newStudent - создание новой сущности типа Student из необработанной строки
 func newStudent(rawData string) (st Student) {
+	items := strings.Split(rawData, " ")
+	st.name = items[0]
+	_age, err := strconv.Atoi(items[1])
+	if err != nil {
+		fmt.Println("Ошибка:", err)
+		_age = 0
+	}
+	st.age = _age
+	_grade, err := strconv.Atoi(items[2])
+	if err != nil {
+		fmt.Println("Ошибка:", err)
+		_grade = 0
+	}
+	st.age = _grade
+	return
+}
+
+func (s Student) studentInfo() (info string) {
+	info = fmt.Sprintf("Student %v (age: %d) of grade %d\n", s.name, s.age, s.grade)
 	return
 }
 
 // main - выполнение основной логики программы
 func main() {
 	for {
-		c := make(chan os.Signal, 1)
-		signal.Notify(c, os.Interrupt)
 
 		inputSource := bufio.NewReader(os.Stdin)
-		rawInput, _ := inputSource.ReadString('\n')
+		rawInput, err := inputSource.ReadString('\n')
+		if err == io.EOF {
+			break
+		}
 		rawInput = strings.Trim(rawInput, "\n")
-		fmt.Printf("->%v<-\n", rawInput)
-
 		fmt.Println("Go down")
-		break
 	}
 }
