d6f54429d961ccb560fb5f42b9634318184cc4c3
Alexander Dubrovin <da070116@gmail.com>
2022-09-05 16:08:02 +0300

    WIP: Add command string args to run server

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"skillbox-test/pkg"
@@ -8,6 +10,10 @@ import (
 
 // main - entry point
 func main() {
+	port := flag.Int("port", 8080, "port to launch server")
+	host := flag.String("host", "localhost", "host to launch server")
+
+	flag.Parse()
 
 	mux := http.NewServeMux()
 	service := pkg.Service{Storage: make(map[int]pkg.User)}
@@ -15,8 +21,11 @@ func main() {
 	mux.HandleFunc("/users/", func(w http.ResponseWriter, r *http.Request) {
 		pkg.HandlersManager(w, r, &service)
 	})
+	addr := fmt.Sprintf("%s:%d", *host, *port)
 
-	err := http.ListenAndServe("localhost:8080", mux)
+	println("Launch server on " + addr)
+
+	err := http.ListenAndServe(addr, mux)
 	if err != nil {
 		log.Fatalln(err)
 	}
