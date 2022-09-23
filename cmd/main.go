89d5a044130162d3d2fa861a4d30e37ada03da78
Dubrovin Alexander <da070116@gmail.com>
2022-09-24 00:38:33 +0300

    Add logic for in-memory storage

--- cmd/main.go
+++ cmd/main.go
@@ -1,73 +1,21 @@
 package main
 
 import (
-	"context"
-	"flag"
-	"github.com/sirupsen/logrus"
-	"github.com/spf13/viper"
-	"net/http"
-	"os"
-	"os/signal"
-	gopherinskillbox "skillbox-test"
+	"fmt"
 	"skillbox-test/pkg"
-	"skillbox-test/pkg/handler"
-	"skillbox-test/pkg/repository"
-	"skillbox-test/pkg/service"
-	"syscall"
 )
 
-// main - entry point
 func main() {
+	sc := pkg.NewStorageCache()
+	filename := "cities.csv"
+	sc.LoadFromFile(filename)
 
-	logrus.SetFormatter(new(logrus.JSONFormatter))
+	sib := sc.FilterCitiesByDistrict("Сибирский")
+	fmt.Println(sib)
 
-	if err := initConfig(); err != nil {
-		logrus.Fatalf("Error in config load: %s", err.Error())
-	}
+	year_gt_1700 := sc.FilterCitiesByFoundationRange(1700, 2000)
+	fmt.Println(year_gt_1700)
 
-	// get args from console to define port and host
-	port := flag.Int("port", viper.GetInt("port"), "port to launch server")
-	host := flag.String("host", "localhost", "host to launch server")
-	flag.Parse()
-
-	db, err := repository.NewSqliteDB(repository.Config{
-		Name: viper.GetString("db_name"),
-	})
-
-	if err != nil {
-		logrus.Fatalf("Failed to init database: %s", err.Error())
-	}
-	defer pkg.CloseDB(db)
-
-	server := new(gopherinskillbox.Server)
-	repos := repository.NewRepository(db)
-	services := service.NewService(repos)
-	handlers := handler.NewHandler(services)
-
-	go func() {
-		err = server.Run(handlers.InitRoutes(), *host, *port)
-		if err != nil && err != http.ErrServerClosed {
-			logrus.Fatalf("Error on server start:%s", err.Error())
-			return
-		}
-	}()
-
-	logrus.Printf("Server on %s and port %d started", *host, *port)
-
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT, os.Interrupt)
-
-	<-quit
-
-	err = server.Shutdown(context.Background())
-	if err != nil {
-		logrus.Fatalf("Error during shutdown: %s", err.Error())
-	}
-}
-
-// initConfig - use configuration files
-func initConfig() error {
-	viper.AddConfigPath("../configs")
-	viper.SetConfigName("config")
-	return viper.ReadInConfig()
+	sc.SetCity("1,Тестовск,Тестовский район,Задачный,0,2022")
+	sc.LoadIntoFile("cities2.csv")
 }
---
+++ cmd/main2.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"net/http"
+	"os"
+	"os/signal"
+	gopherinskillbox "skillbox-test"
+	"skillbox-test/pkg"
+	"skillbox-test/pkg/handler"
+	"skillbox-test/pkg/repository"
+	"skillbox-test/pkg/service"
+	"syscall"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+// main - entry point
+func main2() {
+
+	logrus.SetFormatter(new(logrus.JSONFormatter))
+
+	if err := initConfig(); err != nil {
+		logrus.Fatalf("Error in config load: %s", err.Error())
+	}
+
+	// get args from console to define port and host
+	port := flag.Int("port", viper.GetInt("port"), "port to launch server")
+	host := flag.String("host", "localhost", "host to launch server")
+	flag.Parse()
+
+	db, err := repository.NewSqliteDB(repository.Config{
+		Name: viper.GetString("db_name"),
+	})
+
+	if err != nil {
+		logrus.Fatalf("Failed to init database: %s", err.Error())
+	}
+	defer pkg.CloseDB(db)
+
+	server := new(gopherinskillbox.Server)
+	repos := repository.NewRepository(db)
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
+	go func() {
+		err = server.Run(handlers.InitRoutes(), *host, *port)
+		if err != nil && err != http.ErrServerClosed {
+			logrus.Fatalf("Error on server start:%s", err.Error())
+			return
+		}
+	}()
+
+	logrus.Printf("Server on %s and port %d started", *host, *port)
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT, os.Interrupt)
+
+	<-quit
+
+	err = server.Shutdown(context.Background())
+	if err != nil {
+		logrus.Fatalf("Error during shutdown: %s", err.Error())
+	}
+}
+
+// initConfig - use configuration files
+func initConfig() error {
+	viper.AddConfigPath("../configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
+}
